b10ac69ee09f0d039cad57714e3be95952354cef
unknown <iamrafsal97@gmail.com>
2020-10-08 23:09:26 +0530

    08/10/2020 first enter for multiplication

---
+++ ConsoleMultiplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMultiplication
+{
+    class multiTable
+    {
+        static void multiplication(int N, int i)
+        {
+            if (i > 10)
+                return;
+            Console.WriteLine(N + " * " + i + " = " + N * i);
+            multiplication(N, i + 1);
+        }
+        static void Main(string[] args)
+        {
+            int N = 8;
+            multiplication(N, 1);
+            Console.ReadKey();
+        }
+    }
+}
